0b590fbc92f675e1141b77af68e83172efd65964
sreenath k k <sreenathkk00@gmail.com>
2025-06-25 20:57:55 +0530

    atribute name updated

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -245,9 +245,9 @@ const Contact = () => {
         
         <div className="h-96 rounded-2xl overflow-hidden shadow-lg border border-primary-accent/10">
           <iframe 
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3077.6841848514887!2d75.8030430737993!3d11.2358719889422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba6596ca4d1cb11%3A0xa76f81f9ba68fee1!2sAcharya%20professional%20accountants%20pvt%20ltd!5e1!3m2!1sen!2sin!4v1750755342483!5m2!1sen!2sin" className="w-full h-full border-0 filter grayscale-30 contrast-110" allowfullscreen
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3077.6841848514887!2d75.8030430737993!3d11.2358719889422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba6596ca4d1cb11%3A0xa76f81f9ba68fee1!2sAcharya%20professional%20accountants%20pvt%20ltd!5e1!3m2!1sen!2sin!4v1750755342483!5m2!1sen!2sin" className="w-full h-full border-0 filter grayscale-30 contrast-110" allowFullScreen
           loading="lazy" 
-          referrerpolicy="no-referrer-when-downgrade">
+          referrerPolicy="no-referrer-when-downgrade">
           </iframe>
         </div>
       </div>
